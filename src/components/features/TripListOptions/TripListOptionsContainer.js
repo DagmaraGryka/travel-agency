8bd5d531669720aa59594fff3a3ef41cd0e126fe
Dagmara Gryka <dagagryka@gmail.com>
2021-03-06 10:03:53 +0100

    tags not working

--- src/components/features/TripListOptions/TripListOptionsContainer.js
+++ src/components/features/TripListOptions/TripListOptionsContainer.js
@@ -1,16 +1,22 @@
 import {connect} from 'react-redux';
 import TripListOptions from './TripListOptions';
 import {getAllTags} from '../../../redux/tagsRedux';
-import {getAllFilters, changeSearchPhrase} from '../../../redux/filtersRedux';
+import {getAllFilters,
+  changeSearchPhrase,
+  addTag,
+  removeTag} from '../../../redux/filtersRedux';
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
   tags: getAllTags(state),
   filters: getAllFilters(state),
 });
 
-const mapDispatchToProps = dispatch => ({
+const mapDispatchToProps = (dispatch) => ({
   changeSearchPhrase: phrase => dispatch(changeSearchPhrase(phrase)),
   // TODO - add more dispatchers for other filters
+  addTag: tag => dispatch(addTag(tag)),
+  removeTag: tag => dispatch(removeTag(tag)),
+
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TripListOptions);
