c3274bfcbc7d12124c6f248269fe10f0372ae152
Dagmara Gryka <dagagryka@gmail.com>
2021-03-06 10:37:37 +0100

    fix tags

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -14,6 +14,16 @@ export const getFilteredTrips = ({trips, filters}) => {
   // TODO - filter by duration
 
   // TODO - filter by tags
+  if (filters.tags.length != 0) {
+    output = output.filter(trip => {
+      for(let tag of trip.tags) {
+        if(filters.tags.indexOf(tag) > -1) {
+          return trip;
+        }
+      }
+    });
+  }
+
 
   // TODO - sort by cost descending (most expensive goes first)
 
